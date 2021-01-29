37d23fb4d4c6d9d7518e7e25e65c8c69f4a69048
Shawn CK.P <snowy213@outlook.com>
2021-01-29 14:39:07 +0900

    "feat: added chance to iterate 3 times"

--- numberguess.py
+++ numberguess.py
@@ -3,15 +3,22 @@ import random
 
 answer = random.randint(1,100)
 print('DEBUG: {}'.format(answer))
+chance = 3
 
-guess = int(input('Guess the number(1-100): '))
-print(guess, type(guess))
+while chance > 0:
+    guess = int(input('Guess the number(1-100): '))
+    print(guess, type(guess))
 
-if guess-answer==0:
-    print('Correct!')
-elif guess<answer:
-        print('Up!')
-elif guess>answer:
-        print('Down!')
-        
-print('Wrong! The answer was {}.'.format(answer))
+    if guess-answer==0:
+        print('Correct!')
+        break
+    elif guess<answer:
+        msg = 'Up!'
+    elif guess>answer:
+        msg = 'Down!'
+    chance-=1
+    print('{} You have {} life(s).'.format(msg, chance))
+    
+    if chance ==0:
+        print('You now dead! The answer was {}.'.format(answer))
+
